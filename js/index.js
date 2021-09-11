d4100d76db30aad8fba36db71501a58138ea431a
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2021-09-11 17:33:49 -0500

    styling the input

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ const result = [...document.querySelectorAll(".pricing__total")];
 
 inputCheckbox.addEventListener("change", (e) => {
   if (e.target.checked) {
+    result[0].innerHTML = `<span class="dollar">$</span>19.99</span>`;
+    result[1].innerHTML = `<span class="dollar">$</span>24.99</span>`;
+    result[2].innerHTML = `<span class="dollar">$</span>39.99</span>`;
+  } else {
     for (const r of result) {
       let dollar = parseFloat(r.textContent.slice(1));
       let outputAnnually = `<span class="dollar">$</span>${(
@@ -11,9 +15,5 @@ inputCheckbox.addEventListener("change", (e) => {
 
       r.innerHTML = outputAnnually;
     }
-  } else {
-    result[0].innerHTML = `<span class="dollar">$</span>19.99</span>`;
-    result[1].innerHTML = `<span class="dollar">$</span>24.99</span>`;
-    result[2].innerHTML = `<span class="dollar">$</span>39.99</span>`;
   }
 });
